deeb8da13f1dd7a538d528ee973dceddf0119a4d
Phong Nguyen <hong-phong.nguyen@atos.net>
2023-12-26 14:43:16 +0100

    added jailBreakCheck but commented out

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:pwsafe/fixValues/mytheme.dart';
 import 'package:pwsafe/pages/pwlist.dart';
 import 'package:pwsafe/provider/pw_provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
 
 Future main() async {
   await dotenv.load(fileName: '.env');
@@ -14,8 +16,25 @@ Future main() async {
       child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+/*
+  bool _jailbroken = false;
+  bool _developerMode = false;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _checkJailBreak();
+  }
+*/
 
   @override
   Widget build(BuildContext context) {
@@ -24,4 +43,27 @@ class MyApp extends StatelessWidget {
       home: const PWList(),
     );
   }
+/*
+  Future<void> _checkJailBreak() async {
+    bool jailbroken;
+    bool developerMode;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      jailbroken = await FlutterJailbreakDetection.jailbroken;
+      developerMode = await FlutterJailbreakDetection.developerMode;
+    } on PlatformException {
+      jailbroken = true;
+      developerMode = true;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _jailbroken = jailbroken;
+      _developerMode = developerMode;
+    });
+  }*/
 }
